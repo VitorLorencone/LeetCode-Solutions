bd13b77265696d32d7ef5bbba1afb38bd7208494
Vitor <vitorlorencone@gmail.com>
2024-02-12 22:46:19 -0300

    Done p169

---
+++ Problem 169/solution.py
@@ -0,0 +1,15 @@
+"""
+
+Time Complexity: nlog(n) - worst
+
+Solution with only one line.
+
+"""
+
+class Solution:
+    def majorityElement(self, a: list[int]) -> int:
+        return sorted(a)[len(a)//2]
+    
+sol:Solution = Solution()
+prob:list[int] = [2,2,1,1,1,2,2]
+print(sol.majorityElement(prob))
