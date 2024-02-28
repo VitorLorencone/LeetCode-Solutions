2ac500320109a293b0e65fa95ce1ae3244ff69f4
Vitor <vitorlorencone@gmail.com>
2024-02-28 15:58:23 -0300

    Done cpp

--- Problem513/solution.cpp
+++ Problem513/solution.cpp
@@ -4,7 +4,7 @@ Let n be the number of nodes in the tree and h be the height of the tree
 Time Complexity: O(n)
 Space Complexity: O(h)
 
-A simple and easy recursive solution.
+A recursive solution using dfs.
 
 */
 
@@ -22,34 +22,35 @@ struct TreeNode {
 
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q){
-        if(p == nullptr && q == nullptr){
-            return true;
-        }else if(p == nullptr || q == nullptr){
-            return false;
+    void dfs(TreeNode* node, int level = 0){
+        if(node == nullptr){return;}  
+        if(level > currentLevel){
+            currentLevel = level;
+            value = node->val;
         }
-        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        dfs(node->left, level+1);
+        dfs(node->right, level+1);
     }
+
+    int findBottomLeftValue(TreeNode* root) {
+        value = root->val;
+        dfs(root);
+        return value;
+    }
+private:
+    int value = 0;
+    int currentLevel = 0;
 };
 
 int main(){
 
     Solution sol = Solution();
-    TreeNode* p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
-    TreeNode* q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+    TreeNode* root = new TreeNode(1, new TreeNode(2, new TreeNode(4), nullptr), new TreeNode(3, new TreeNode(5, new TreeNode(7), nullptr), new TreeNode(6)));
 
-    cout << sol.isSameTree(p, q) << endl;
+    cout << sol.findBottomLeftValue(root) << endl;
 
-    p = new TreeNode(1, new TreeNode(2), nullptr);
-    q = new TreeNode(1, nullptr, new TreeNode(2));
-
-    cout << sol.isSameTree(p, q) << endl;
-
-    delete p->right;
-    delete p->left;
-    delete p;
-    delete q->right;
-    delete q->left;
-    delete q;
+    delete root->right;
+    delete root->left;
+    delete root;
 
 }
