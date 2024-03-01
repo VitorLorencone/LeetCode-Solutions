f924f7f0f50d18b759ca956c5529078df1284467
Vitor <vitorlorencone@gmail.com>
2024-02-29 21:45:41 -0300

    Done p1609

--- Problem1609/solution.py
+++ Problem1609/solution.py
@@ -2,7 +2,7 @@
 Let n be the number of nodes in the tree and h be the height of the tree
 
 Time Complexity: O(n)
-Space Complexity: O(h)
+Space Complexity: O(n) -> values vector use O(h) but the worst case in call stack is O(n)
 
 A recursive solution using dfs.
 
@@ -16,25 +16,30 @@ class TreeNode:
 
 class Solution:
 
-    value:int = 0
-    currentLevel:int = 0
+    solution:bool = True
 
-    def dfs(self, node:TreeNode, level = 0):
-        if node is None:
-            return
-    
-        if level > self.currentLevel:
-            self.value = node.val
-            self.currentLevel = level
+    def isEvenOddTree(self, root: TreeNode) -> bool:
+        values:list = []
 
-        self.dfs(node.left, level+1)
-        self.dfs(node.right, level+1)
-    
-    def findBottomLeftValue(self, root: TreeNode) -> int:
-        self.value = root.val
-        self.dfs(root)
-        return self.value
+        def solver(node: TreeNode, level:int = 0) -> None:
+            sign:int = -2*(level%2)+1
+            if node is None or self.solution == False:
+                return
+            if node.val%2 == level%2:
+                self.solution = False
+                return
+            if len(values) <= level:
+                values.append(node.val)
+            elif values[level]*sign < node.val*sign:
+                values[level] = node.val
+            else:
+                self.solution = False
+            solver(node.left, level+1)
+            solver(node.right, level+1)
+
+        solver(root)
+        return self.solution
     
 sol:Solution = Solution()
-root = TreeNode(1, TreeNode(2, TreeNode(4), None), TreeNode(3, TreeNode(5, TreeNode(7, None)), TreeNode(6)))
-print(sol.findBottomLeftValue(root))
+root:TreeNode = TreeNode(5, TreeNode(4, TreeNode(3), TreeNode(5)), TreeNode(2, TreeNode(7), None))
+print(sol.isEvenOddTree(root))
