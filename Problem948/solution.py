15f1fe88d64ceef4c10264f6f0b1b03166cb9a90
Vitor <vitorlorencone@gmail.com>
2024-03-04 17:00:08 -0300

    Done p948

--- Problem948/solution.py
+++ Problem948/solution.py
@@ -1,33 +1,37 @@
 """ 
-Let n be the number of nodes in the tree and h be the height of the tree
+Let n be len(tokens)
 
-Time Complexity: O(n)
-Space Complexity: O(h)
+Time Complexity: O(nlog(n))
+Space Complexity: O(1)
 
-A simple and easy recursive solution.
+A solution using greedy, two pointers and sort.
 
 """
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if p is None and q is None:
-            return True
-        elif p is None or q is None:
-            return False
-    
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) and p.val == q.val
+    def bagOfTokensScore(self, tokens: list[int], power: int) -> int:
+        tokens.sort()
+        points:int = 0
+        maxPoints:int = 0
+        p:int = 0
+        q:int = len(tokens)-1
+        
+        for i in range(len(tokens)):
+            if(tokens[p] <= power):
+                power -= tokens[p]
+                p += 1
+                points += 1
+                maxPoints = max(maxPoints, points)
+                continue
+            elif(points > 0):
+                power += tokens[q]
+                q -= 1
+                points -= 1
+                continue
+            break
+        return maxPoints
     
 sol:Solution = Solution()
-p = TreeNode(1, TreeNode(2), TreeNode(3))
-q = TreeNode(1, TreeNode(2), TreeNode(3))
-print(sol.isSameTree(p, q))
-
-p = TreeNode(1, None, TreeNode(2))
-q = TreeNode(1, TreeNode(2), None)
-print(sol.isSameTree(p, q))
+tks:list = [100,200,300,400]
+pwr:int = 200
+print(sol.bagOfTokensScore(tks, pwr))
