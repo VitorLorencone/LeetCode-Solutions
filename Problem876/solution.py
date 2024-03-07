65a14757c211545008b7febe85566c57814edbe2
Vitor <vitorlorencone@gmail.com>
2024-03-06 22:02:13 -0300

    Done py

--- Problem876/solution.py
+++ Problem876/solution.py
@@ -1,33 +1,30 @@
 """ 
-Let n be the number of nodes in the tree and h be the height of the tree
+Let n be the number of nodes in the list
 
 Time Complexity: O(n)
-Space Complexity: O(h)
+Space Complexity: O(1)
 
-A simple and easy recursive solution.
+A simple and easy two pointers solution
 
 """
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+class ListNode:
+    def __init__(self, val=0, next=None):
         self.val = val
-        self.left = left
-        self.right = right
+        self.next = next
 
 class Solution:
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if p is None and q is None:
-            return True
-        elif p is None or q is None:
-            return False
-    
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) and p.val == q.val
+    def middleNode(self, head: ListNode) -> ListNode:
+        p = head
+        q = head
+        while p.next is not None:
+            if p.next.next is not None:
+                p = p.next.next
+            else:
+                p = p.next
+            q = q.next
+        return q
     
 sol:Solution = Solution()
-p = TreeNode(1, TreeNode(2), TreeNode(3))
-q = TreeNode(1, TreeNode(2), TreeNode(3))
-print(sol.isSameTree(p, q))
-
-p = TreeNode(1, None, TreeNode(2))
-q = TreeNode(1, TreeNode(2), None)
-print(sol.isSameTree(p, q))
+p = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6))))))
+print(sol.middleNode(p).val)
