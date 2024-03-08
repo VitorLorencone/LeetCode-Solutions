c0a6e735e7a3e248255c9abb99f0130d82c1c01d
Vitor <vitorlorencone@gmail.com>
2024-03-07 23:48:02 -0300

    Done p3005

--- Problem3005/Description.md
+++ Problem3005/Description.md
@@ -1,26 +1,24 @@
 # **[3005. Count Elements With Maximum Frequency](https://leetcode.com/problems/count-elements-with-maximum-frequency/description/)**
 
-Given the **head** of a singly linked list, return *the middle node of the linked list.*
+You are given an array `nums` consisting of **positive** integers.
 
-If there are two middle nodes, return **the second middle** node.
+Return *the **total frequencies** of elements in* `nums` *such that those elements all have the **maximum** frequency.*
+
+The **frequency** of an element is the number of occurrences of that element in the array.
 
 **Example 1:**
 
-![ex1](https://assets.leetcode.com/uploads/2021/07/23/lc-midlist1.jpg)
-
-> **Input:** head = [1,2,3,4,5]  
-> **Output:** [3,4,5]  
-> **Explanation:** The middle node of the list is node 3.
+> **Input:** nums = [1,2,2,3,1,4]  
+> **Output:** 4  
+> **Explanation:** The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array. So the number of elements in the array with maximum frequency is 4.
 
 **Example 2:**
 
-![ex2](https://assets.leetcode.com/uploads/2021/07/23/lc-midlist2.jpg)
-
-> **Input:** head = [1,2,3,4,5,6]  
-> **Output:** [4,5,6]  
-> **Explanation:** Since the list has two middle nodes with values 3 and 4, we return the second one.
+> **Input:** nums = [1,2,3,4,5]  
+> **Output:** 5  
+> **Explanation:** All elements of the array have a frequency of 1 which is the maximum. So the number of elements in the array with maximum frequency is 5.
 
 **Constraints:**
 
-- The number of nodes in the list is in the range `[1, 100]`.
-- `1 <= Node.val <= 100`
+- `1 <= nums.length <= 100`
+- `1 <= nums[i] <= 100`
--- Problem3005/solution.cpp
+++ Problem3005/solution.cpp
@@ -1,49 +1,44 @@
 /* 
-Let n be the number of nodes in the list
+Let n be the size of the list nums
 
 Time Complexity: O(n)
-Space Complexity: O(1)
+Space Complexity: O(n)
 
-A simple and easy two pointers solution.
+A solution using hash map for counting.
 
 */
 
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
- 
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode* p = head;
-        ListNode* q = head;
-        while(p->next != nullptr){
-            if(p->next->next != nullptr){
-                p = p->next->next;
-            }else{
-                p = p->next;
-            }
-            q = q->next;
+    int maxFrequencyElements(vector<int>& nums) {
+        int maxFreq = 0;
+        int ans = 0;
+        std::unordered_map<int, int> dict;
+
+        for(int i = 0; i < nums.size(); i++){
+            dict[nums[i]] += 1;
         }
-        return q;
+
+        for(const auto& pair : dict){
+            if(pair.second > maxFreq){
+                maxFreq = pair.second;
+                ans = pair.second;
+            }else if(pair.second == maxFreq){
+                ans += pair.second;
+            }
+        }
+        
+        return ans;
     }
 };
 
 int main(){
-
     Solution sol = Solution();
-    ListNode* p = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
-
-    cout << sol.middleNode(p)->val << endl;
-
-    delete p->next;
-    delete p;
-
+    vector<int> p = {1, 2, 3, 4, 5};
+    cout << sol.maxFrequencyElements(p) << endl;
 }
