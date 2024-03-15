c3beb4d6049ba61365909986e9f52f7f2e6459dd
Vitor <vitorlorencone@gmail.com>
2024-03-15 02:10:52 -0300

    Done p238

--- Problem238/solution.cpp
+++ Problem238/solution.cpp
@@ -2,37 +2,32 @@
 Let n be the size of the list nums
 
 Time Complexity: O(n)
-Space Complexity: O(n)
+Space Complexity: O(n) -> Including the vector "ans";
 
-A solution using hash map for counting.
+Simple solution with two for loops calculating left and right multiplications.
 
 */
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
-    int maxFrequencyElements(vector<int>& nums) {
-        int maxFreq = 0;
-        int ans = 0;
-        std::unordered_map<int, int> dict;
-
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int val = 1;
+        vector<int> ans(nums.size(), 0);
         for(int i = 0; i < nums.size(); i++){
-            dict[nums[i]] += 1;
-        }
-
-        for(const auto& pair : dict){
-            if(pair.second > maxFreq){
-                maxFreq = pair.second;
-                ans = pair.second;
-            }else if(pair.second == maxFreq){
-                ans += pair.second;
-            }
+            ans[i] = val;
+            val *= nums[i];
         }
         
+        val = 1;
+        for(int i = nums.size()-1; i >= 0; i--){
+            ans[i] *= val;
+            val *= nums[i];
+        }
+
         return ans;
     }
 };
@@ -40,5 +35,9 @@ public:
 int main(){
     Solution sol = Solution();
     vector<int> p = {1, 2, 3, 4, 5};
-    cout << sol.maxFrequencyElements(p) << endl;
+    vector<int> answer = sol.productExceptSelf(p);
+    for(int i = 0; i < answer.size(); i++){
+        cout << answer[i] << endl;
+    }
+
 }
