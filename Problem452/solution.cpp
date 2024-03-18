f83b4d609c89a2d7798f98870e3f58d7be43a63f
Vitor <vitorlorencone@gmail.com>
2024-03-18 18:19:57 -0300

    Done p452

--- Problem452/solution.cpp
+++ Problem452/solution.cpp
@@ -1,43 +1,39 @@
 /* 
-Let n be the size of the list nums
+Let n be the size of the list *points*
 
-Time Complexity: O(n)
-Space Complexity: O(n) -> Including the vector "ans";
+Time Complexity: O(n*log(n))
+Space Complexity: O(1)
 
-Simple solution with two for loops calculating left and right multiplications
+Simple solution with pre-processing sort and greedy algorithm.
 
 */
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int val = 1;
-        vector<int> ans(nums.size(), 0);
-        for(int i = 0; i < nums.size(); i++){
-            ans[i] = val;
-            val *= nums[i];
-        }
-        
-        val = 1;
-        for(int i = nums.size()-1; i >= 0; i--){
-            ans[i] *= val;
-            val *= nums[i];
-        }
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end());
 
-        return ans;
+        int arrows = 0;
+        vector<int> lastPoint = {-1, -1};
+        for(int i = 0; i < points.size(); i++){
+            if(points[i][0] >= lastPoint[0] && points[i][0] <= lastPoint[1]){
+                lastPoint[1] = min(lastPoint[1], points[i][1]);
+                continue;
+            }
+            arrows++;
+            lastPoint = points[i];
+        }
+        return arrows;
     }
 };
 
 int main(){
     Solution sol = Solution();
-    vector<int> p = {1, 2, 3, 4, 5};
-    vector<int> answer = sol.productExceptSelf(p);
-    for(int i = 0; i < answer.size(); i++){
-        cout << answer[i] << endl;
-    }
-
+    vector<vector<int>> p = {{1,2},{2,3},{3,4},{4,5}};
+    cout << sol.findMinArrowShots(p);
 }
