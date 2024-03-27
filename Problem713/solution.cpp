7919690a9bc756980bec3ebee1dca41acea7164c
Vitor <vitorlorencone@gmail.com>
2024-03-27 13:12:33 -0300

    Done p713

--- Problem713/solution.cpp
+++ Problem713/solution.cpp
@@ -3,9 +3,6 @@ Let n be the size of *nums*
 
 Time Complexity: O(n)
 Space Complexity: O(1)
-
-Two pointer solution.
-
 */
 
 #include <iostream>
@@ -13,27 +10,26 @@ Two pointer solution.
 
 class Solution {
 public:
-    int findDuplicate(std::vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        slow = nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+    int numSubarrayProductLessThanK(std::vector<int>& nums, int k) {
+        if (k == 0)
+            return 0;
+        int n = nums.size(), product = 1, cnt {};
+        int l = 0, r = 0;
+        while(r < n)
+        {
+            product *= nums[r++];
+            while(l < r && product >= k)
+                product /= nums[l++];
+            
+            cnt += (r - l);
         }
-
-        return slow;
+        return cnt;
     }
 };
 
 int main(){
     Solution sol = Solution();
-    std::vector<int> nums = {1,2,6,5,4,3,2};
-    std::cout << sol.findDuplicate(nums);
+    std::vector<int> nums = {10, 5, 2, 6};
+    int k = 100;
+    std::cout << sol.numSubarrayProductLessThanK(nums, k);
 }
