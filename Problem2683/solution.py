a2e32d68128393c5279c703a0eb541fca1fe881a
Vitor <vitorlorencone@gmail.com>
2025-01-17 21:03:23 -0300

    Done p2683

---
+++ Problem2683/solution.py
@@ -0,0 +1,18 @@
+"""
+let n be the length of `derived`
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+"""
+
+class Solution:
+    def doesValidArrayExist(self, derived: list[int]) -> bool:
+        XOR = 0
+        for element in derived:
+            XOR = XOR ^ element
+        return XOR == 0
+    
+sol:Solution = Solution()
+derived:list = [1,1,0]
+print(sol.doesValidArrayExist(derived))
